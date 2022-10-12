6e998ef386d4bd340a87253683f3e91634262c1e
danala <greatprotectionwallet@gmail.com>
2022-10-12 13:02:08 -0600

    add new project starting views and css files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static('public'))
 
 app.use('/admin', adminRouter)
 app.use(shopRouter)
