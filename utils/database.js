e8e7bd4ac81a0f0a969ece6547190b80cf67f150
danala <greatprotectionwallet@gmail.com>
2022-10-17 11:13:58 -0600

    sequelize package installed

--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,17 @@
-const mysql = require('mysql2')
 require('dotenv').config()
-
 const MySqlPassword = process.env.MY_SQL_PASSWORD
+// const mysql = require('mysql2')
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-Udemy',
+//     password: MySqlPassword,
+// })
+// module.exports = pool.promise()
 
-const pool = mysql.createPool({
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node-Udemy', 'root', MySqlPassword, {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-Udemy',
-    password: MySqlPassword,
 })
-
-module.exports = pool.promise()
+module.exports = sequelize
